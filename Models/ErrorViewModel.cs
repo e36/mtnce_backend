e998ba9e44e4e05be2a9226a2ebacda4918113d9
e36 <agent4159@gmail.com>
2021-12-12 13:47:06 -0600

    initial create

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mtnce_backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
